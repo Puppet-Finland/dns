14790c7ec02cbcad15b6038016671d7b0f34e669
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-03-07 08:43:45 +0200

    Added support for allowing tcp/53 in dns::packetfilter
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/packetfilter.pp
+++ manifests/packetfilter.pp
@@ -4,35 +4,68 @@
 # This class configures packetfilter to only let in traffic from specified 
 # IP-addresses to the dns daemon (e.g. bind, dnsmasq).
 #
+# == Parameters
+#
+# [*allow_tcp*]
+#   Allow DNS requests to TCP port 53. This may be necessary for many reasons 
+#   (DNSSec, zone transfers, IPv6). Valid values are 'yes' (default) and 'no'.
+# [*allow_address_ipv4*]
+#   An IPv4 address or subnet from which to allow connections.
+# [*allow_address_ipv6*]
+#   An IPv6 address or subnet from which to allow connections.
+#
 class dns::packetfilter
 (
+    $allow_tcp = 'yes',
     $allow_address_ipv4,
     $allow_address_ipv6
 
 ) inherits dns::params
 {
 
-    firewall { "007 ipv4 accept dns":
-        provider => 'iptables',
+    # Determine whether to limit access or not
+    $source_v4 = $allow_address_ipv4 ? {
+        'any' => undef,
+        default => $allow_address_ipv4,
+     }
+
+    $source_v6 = $allow_address_ipv6 ? {
+        'any' => undef,
+        default => $allow_address_ipv6,
+     }
+
+    # Resource defaults
+    Firewall {
         chain => 'INPUT',
-        proto => 'udp',
         action => 'accept',
-        source => $allow_address_ipv4 ? {
-            'any' => undef,
-            default => $allow_address_ipv4,
-        },
         dport => 53,
     }
 
-    firewall { "007 ipv6 accept dns":
-        provider => 'ip6tables',
-        chain => 'INPUT',
+    # UDP rules
+    firewall { "007 ipv4 accept udp dns":
+        provider => 'iptables',
         proto => 'udp',
-        action => 'accept',
-        source => $allow_address_ipv6 ? {
-            'any' => undef,
-            default => $allow_address_ipv6,
-        },
-        dport => 53,
+        source => $source_v4,
+    }
+
+    firewall { "007 ipv6 accept udp dns":
+        provider => 'ip6tables',
+        proto => 'udp',
+        source => $source_v6,
+    }
+
+    # TCP rules
+    if $allow_tcp == 'yes' {
+        firewall { "007 ipv4 accept tcp dns":
+            provider => 'iptables',
+            proto => 'tcp',
+            source => $source_v4,
+        }
+
+        firewall { "007 ipv6 accept tcp dns":
+            provider => 'ip6tables',
+            proto => 'tcp',
+            source => $source_v6,
+        }
     }
 }
